1f76aaea0d3e97e0ded0a9c34dfbc0973b1bcce9
plevagen <varkginglouh@gmail.com>
2026-04-26 17:46:48 +0500

    fdgsrogrg

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,32 @@ public class Main {
         Sorter sorter = new Sorter();
         Searcher searcher = new Searcher();
         Experiment experiment = new Experiment();
+
+        System.out.println("Demonstration of algorithms on little example");
+
+        int[] demo = sorter.generateRandomArray(10);
+
+        System.out.print("Default array: ");
+        sorter.printArray(demo);
+
+        int[] demoForSelection = sorter.copyArray(demo);
+        sorter.basicSort(demoForSelection);
+        System.out.print("After Selection Sort: ");
+        sorter.printArray(demoForSelection);
+
+        int[] demoForQuick = sorter.copyArray(demo);
+        sorter.advancedSort(demoForQuick);
+        System.out.print("After Quick Sort: ");
+        sorter.printArray(demoForQuick);
+
+        int target = demoForQuick[5];
+        int foundIndex = searcher.search(demoForQuick, target);
+        System.out.println("Binary search: looking" + target + "-> index" + foundIndex);
+
+        int notFound = searcher.search(demoForQuick, -999);
+        System.out.println("Binary search: index " + notFound + "is not found");
+
+        System.out.println();
+        experiment.runAllExperiments();
     }
 }
