ac09e954f385692ec32ee1e9f827794022866724
plevagen <varkginglouh@gmail.com>
2026-04-27 16:36:20 +0500

    fdgsrogrg

--- src/Sorter.java
+++ src/Sorter.java
@@ -51,11 +51,11 @@ public class Sorter {
     }
 
     public void printArray(int[] arr){
-        System.out.println("[");
+        System.out.print("[");
         for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
             if(i < arr.length - 1){
-                System.out.println(",");
+                System.out.print(",");
             }
         }
         System.out.println("]");
